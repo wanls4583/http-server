3591e3e58b2815bb2b4a6f45449a119df8fb514f
lisong <409051987@qq.com>
2024-07-13 21:46:35 +0800

    fix: free(head->serverName)

--- TlsUtils.cpp
+++ TlsUtils.cpp
@@ -7,24 +7,20 @@
 
 using namespace std;
 
-TlsUtils::TlsUtils() : ctxHead(NULL), ctxEnd(NULL), maxCtx(100), ctxCount(0)
-{
+TlsUtils::TlsUtils(): ctxHead(NULL), ctxEnd(NULL), maxCtx(100), ctxCount(0) {
     pthread_mutex_init(&certMutex, NULL);
 };
 
-unsigned int TlsUtils::bufToInt(char *buf, int size)
-{
+unsigned int TlsUtils::bufToInt(char* buf, int size) {
     unsigned int res = 0;
-    for (int i = 0; i < size; i++)
-    {
+    for (int i = 0; i < size; i++) {
         res = (res << 8) | u_int8_t(*(buf + i));
     }
     return res;
 }
 
-char *TlsUtils::getServerName(int sock)
-{
-    char buf[5], *pos, *serverName = NULL, *end;
+char* TlsUtils::getServerName(int sock) {
+    char buf[5], * pos, * serverName = NULL, * end;
     short tlsLen = 0;
     unsigned int sessionIdLen = 0, cipherSuitesLen = 0, compMethLen = 0, extType = 0, extLen = 0;
 
@@ -33,7 +29,7 @@ char *TlsUtils::getServerName(int sock)
     memcpy(&tlsLen, buf + 3, 2);
     tlsLen = ntohs(tlsLen);
 
-    pos = (char *)malloc(tlsLen);
+    pos = (char*)malloc(tlsLen);
     memset(pos, 0, tlsLen);
     recv(sock, pos, tlsLen, MSG_PEEK);
     end = pos + tlsLen - 1;
@@ -58,67 +54,64 @@ char *TlsUtils::getServerName(int sock)
     pos += compMethLen;
     pos += TLS_EXT_LEN;
 
-    while (pos < end)
-    {
+    while (pos < end) {
         extType = bufToInt(pos, 2);
         pos += 2;
         extLen = bufToInt(pos, 2);
         pos += 2;
-        if (extType == 0)
-        {
+        if (extType == 0) {
             int nameLen = bufToInt(pos + 3, 2);
-            serverName = (char *)calloc(1, nameLen + 1);
+            serverName = (char*)calloc(1, nameLen + 1);
             memcpy(serverName, pos + 5, nameLen);
             break;
         }
         pos += extLen;
     }
+    
     return serverName;
 }
 
-SSL_CTX *TlsUtils::getCert(int sock)
-{
+SSL_CTX* TlsUtils::getCert(int sock) {
     pthread_mutex_lock(&certMutex);
-    char *serverName = this->getServerName(sock);
-    if (!serverName)
-    {
-        serverName = (char *)"127.0.0.1";
+    char* serverName = this->getServerName(sock);
+    if (!serverName) {
+        serverName = new char[string("127.0.0.1").size() + 1];
+        strcpy(serverName, "127.0.0.1");
     }
-    ServerMap *head = this->ctxHead;
-    while (head)
-    {
-        if (serverName && strcmp(head->serverName, serverName) == 0)
-        {
+
+    ServerMap* head = this->ctxHead;
+
+    while (head) {
+        if (serverName && strcmp(head->serverName, serverName) == 0) {
             pthread_mutex_unlock(&certMutex);
             return head->ctx;
         }
         head = head->next;
     }
-    SSL_CTX *ctx = this->initCert(serverName);
 
-    if (!ctx)
-    {
+    SSL_CTX* ctx = this->initCert(serverName);
+
+    if (!ctx) {
         pthread_mutex_unlock(&certMutex);
         return NULL;
     }
 
-    ServerMap *srvMap = (ServerMap *)malloc(sizeof(ServerMap));
+    ServerMap* srvMap = (ServerMap*)malloc(sizeof(ServerMap));
     srvMap->serverName = serverName;
     srvMap->ctx = ctx;
     srvMap->next = NULL;
-    if (this->ctxEnd)
-    {
+
+    if (this->ctxEnd) {
         this->ctxEnd->next = srvMap;
         this->ctxEnd = srvMap;
-    }
-    else
-    {
+    } else {
         this->ctxHead = srvMap;
         this->ctxEnd = srvMap;
     }
+
     this->ctxCount++;
-    if (this->ctxCount > this->maxCtx)
-    {
+
+    if (this->ctxCount > this->maxCtx) {
         head = this->ctxHead;
         this->ctxCount = this->maxCtx;
         this->ctxHead = head->next;
@@ -127,25 +120,24 @@ SSL_CTX *TlsUtils::getCert(int sock)
         free(head->serverName);
         free(head);
     }
+
     pthread_mutex_unlock(&certMutex);
+
     return ctx;
 }
 
-SSL_CTX *TlsUtils::initCert(char *serverName)
-{
-    EVP_PKEY *pkey;
-    X509 *domainCert;
+SSL_CTX* TlsUtils::initCert(char* serverName) {
+    EVP_PKEY* pkey;
+    X509* domainCert;
 
-    if (!this->certUtils.createCertFromRequestFile(&pkey, &domainCert, serverName))
-    {
+    if (!this->certUtils.createCertFromRequestFile(&pkey, &domainCert, serverName)) {
         return NULL;
     }
 
     SSL_load_error_strings();
     OpenSSL_add_ssl_algorithms();
-    SSL_CTX *ctx = SSL_CTX_new(TLS_server_method());
-    if (!ctx)
-    {
+    SSL_CTX* ctx = SSL_CTX_new(TLS_server_method());
+    if (!ctx) {
         ERR_print_errors_fp(stderr);
         return NULL;
     }
@@ -159,37 +151,32 @@ SSL_CTX *TlsUtils::initCert(char *serverName)
     //     ERR_print_errors_fp(stderr);
     //     exit(4);
     // }
-    if (SSL_CTX_use_certificate(ctx, domainCert) <= 0)
-    {
+    if (SSL_CTX_use_certificate(ctx, domainCert) <= 0) {
         ERR_print_errors_fp(stderr);
         return NULL;
     }
-    if (SSL_CTX_use_PrivateKey(ctx, pkey) <= 0)
-    {
+    if (SSL_CTX_use_PrivateKey(ctx, pkey) <= 0) {
         ERR_print_errors_fp(stderr);
         return NULL;
     }
-    if (!SSL_CTX_check_private_key(ctx))
-    {
+    if (!SSL_CTX_check_private_key(ctx)) {
         fprintf(stderr, "Private key does not match the certificate public key\n");
         return NULL;
     }
     return ctx;
 }
 
-SSL *TlsUtils::getSSL(int sock)
-{
+SSL* TlsUtils::getSSL(int sock) {
     char buf[2];
-    SSL *ssl = NULL;
-    SSL_CTX *ctx = NULL;
+    SSL* ssl = NULL;
+    SSL_CTX* ctx = NULL;
     ctx = this->getCert(sock);
-    if (!ctx)
-    {
+    if (!ctx) {
         return NULL;
     }
-    X509 *client_cert;
+    X509* client_cert;
     int err;
-    char *str;
+    char* str;
     ssl = SSL_new(ctx);
     SSL_set_fd(ssl, sock);
     err = SSL_accept(ssl);
